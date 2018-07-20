80a97af72df635c1d260152da09d7aaf0152acb7
TheBigD6 <40880573+TheBigD6@users.noreply.github.com>
2018-07-20 11:49:00 +0200

    ReadCSVFile

---
+++ ReadFileCSV/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"C:\ReadFileCSV\ReadMe.csv"))
+                {
+                    string line ="";
+
+                    while ((line = sr.ReadLine()) != null)
+	                {
+                        Console.WriteLine(line);
+	                }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The file could not be found: ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
